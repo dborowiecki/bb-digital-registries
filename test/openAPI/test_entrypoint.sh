abc1001ae8e9f07af0b71c7a9057ad898baff4d3
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-01-28 17:48:53 +0100

    TECH-166: Adde openAPI suite test entrypoint

---
+++ test/openAPI/test_entrypoint.sh
@@ -0,0 +1,4 @@
+## Required Global variables used to execute test harness 
+## RESULT_NAME <- determines where test result is stored. 
+## API_URL <- used to detemine base URL of tested API
+docker-compose up --build gherkin-test-report
