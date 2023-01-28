421dc0ebb4d3af48f7df049e18f2a2e6ef3d741c
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-01-28 17:11:00 +0100

    TECH-166: Adjusted config build

--- .circleci/generate-config.py
+++ .circleci/generate-config.py
@@ -3,44 +3,63 @@ import os
 import logging
 import pathlib
 import json 
+import itertools
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 base_config = os.path.join(dir_path, 'base-generated-config.yaml')
 generated_config = os.path.join(dir_path, 'generated.yml')
 examples_path = os.path.join(pathlib.Path(dir_path).parent, "examples")
+test_suites_path = os.path.join(pathlib.Path(dir_path).parent, "test")
 
 
-def validate_example(example_path): 
+def is_valid(test_harness_path): 
   """
-  Example should provide following data:
+  Both example and test suite directories should provide following data:
   - test_entrypoint.sh
-    Used to initialize test application.  
-  - config.json <future requirement>
-    Will be used to configure gherkin test execution for particular application (e.g. URL's, data etc.)
+    Used to initialize test application.
+  
+  If shell script is not provided given entity is excluded from test harness. 
   """
-  elements = [os.path.basename(name) for name in os.listdir(example_path)]
+  elements = [os.path.basename(name) for name in os.listdir(test_harness_path)]
   if 'test_entrypoint.sh' not in elements:
-      print(F'Missing test_entrypoint.sh: {example_path}!')
+      return False 
+  return True 
 
+
+def app_and_name_from_path(test_harness_path): 
+    test_path = str(pathlib.Path(test_harness_path).absolute())
+    test_name = str(os.path.basename(test_path))  
+    return test_path, test_name
+
+def list_test_executions(example_apps, test_suites): 
+  # Test are executed for all cancidate applications tested against all test suites
+  test_executions = []
+  for element in itertools.product(example_apps, test_suites):
+    app, suite = element 
+    if not (is_valid(app) and is_valid(suite)):
+      print(F'---\nTest harness for:\n\tApp: {app}\n\tTest Suite: {suite}"\nwill not be executed.\ntest_entrypoint.sh is missing.\n---')
+      continue
+
+    app_path, app_name = app_and_name_from_path(app)
+    suite_path, suite_name = app_and_name_from_path(suite)
+
+    new_example = {
+      'example-app-path': app_path, 'test-suite-name': suite_name, 
+      'example-app-name': app_name, 'test-suite-path': suite_path, 
+    }
+    test_executions.append({F"{app_name}-{suite_name}": new_example})
+  return test_executions
 
 
 with open(base_config) as f:
     circle_config = yaml.safe_load(f)
     
     available_examples = [name for name in pathlib.Path(examples_path).iterdir() if os.path.isdir(name)]
+    available_test_suites = [name for name in pathlib.Path(test_suites_path).iterdir() if os.path.isdir(name)]
 
     examples = []
-    circle_config['workflows']['test_everything']['jobs'] = []
-    for path in available_examples:
-      validate_example(path)
-      path_str = str(pathlib.Path(path).absolute())
-      name = str(os.path.basename(path))  
-      new_example = {
-        'path': path_str, 
-        'example-app-name': name
-      }
-      circle_config['workflows']['test_everything']['jobs'].append({F'test-example': new_example})
-      
+    circle_config['workflows']['test_everything']['jobs'] = list_test_executions(available_examples, available_test_suites)
+
     with open(generated_config, "w") as w: 
       yaml.dump(circle_config, w, default_flow_style=False)
--- test/openAPI/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-## Required Global variables used to execute test harness 
-## RESULT_NAME <- determines where test result is stored. 
-## API_URL <- used to detemine base URL of tested API
-docker-compose up --build gherkin-test-report
